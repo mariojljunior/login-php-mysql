6439efd31faf81b26790d2686496188922f38f28
Mario Junior <65376066+mariojljunior@users.noreply.github.com>
2020-09-19 13:02:31 -0400

    formulario

--- index.php
+++ index.php
@@ -3,12 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="/css/estilo.css">
+    <title>Projeto Login</title>
 </head>
 <body>
     <div>
-        <form action="#">
-            
+        <form method="POST" action="processa.php">
+            <input type="email">
+            <input type="password">
+            <input type="submit">
         </form>
     </div>
 </body>
