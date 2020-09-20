0d5675e2f425138cc6144e32aa6c21b9247fb8f6
Mario Junior <65376066+mariojljunior@users.noreply.github.com>
2020-09-19 22:21:27 -0400

    função cadstrar

--- classes/usuarios.php
+++ classes/usuarios.php
@@ -1,18 +1,42 @@
 <?php
 
-class Usuario {
+class Usuario{
 
     private $pdo;
+    public $msgErro = "";
+
     public function conectar($nome, $host, $usuario, $senha){
         global $pdo;
+        global $msgErro;
+
+        try{
+            $pdo = new PDO("mysql:dbname=".$nome.";host=".$host.";usuario=".$usuario.";senha=".$senha);
+        } catch (PDOException $e) {
+            $msgErro = $e->getMessage();
+        }
     }
 
-    public function cadastrar(){
+    public function cadastrar($nome, $telefone, $email, $senha){
         global $pdo;
 
+        //verificar se já existe o email cadastrado
+        $sql = $pdo->prepare("SELECT id_usuario FROM usuarios WHERE email = :e");
+        $sql->blindValue(":e",$email);
+        $sql->execute();
+        if($sql->rowCount() > 0){
+            return false;//já está cadastrado
+        }else{//Caso não, Cadastrar.
+            $sql = $pdo->prepare("INSERT INTO usuarios (nome, telefone, email, senha) VALUES (:n, :t, :e, :s)");
+            $sql->blindValue(":n",$nome);
+            $sql->blindValue(":t",$telefone);
+            $sql->blindValue(":e",$email);
+            $sql->blindValue(":s",$senha);
+            $sql->execute();
+            return true;
+        }
     }
 
-    public function logar(){
+    public function logar($email, $senha){
         global $pdo;
 
     }
