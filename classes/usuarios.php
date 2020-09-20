5f81543db86d6d44a8af41a096fbefe33d2ab7b1
Mario Junior <65376066+mariojljunior@users.noreply.github.com>
2020-09-19 21:49:41 -0400

    php conexão

---
+++ classes/usuarios.php
@@ -0,0 +1,22 @@
+<?php
+
+class Usuario {
+
+    private $pdo;
+    public function conectar($nome, $host, $usuario, $senha){
+        global $pdo;
+    }
+
+    public function cadastrar(){
+        global $pdo;
+
+    }
+
+    public function logar(){
+        global $pdo;
+
+    }
+}
+
+
+?>
--- index.php
+++ index.php
@@ -10,8 +10,8 @@
     <div id="corpo-form">
         <h1>Entrar</h1>
         <form method="POST" action="processa.php">
-            <input type="email" placeholder="Usuário">
-            <input type="password" placeholder="Senha">
+            <input type="email" name="email" placeholder="Usuário">
+            <input type="password" name="senha" placeholder="Senha">
             <input type="submit" value="ACESSAR">
             <a href="cadastrar.php">Ainda não é inscrito? <strong>Cadastre-se!</strong></a>
         </form>
